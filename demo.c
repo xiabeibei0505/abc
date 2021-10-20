475696160bf89e084d793f5f3e9b338f30c9ff9e
Xia Beibei <xiabeibei163@163.com>
2021-10-20 21:38:05 +0800

    submit

--- demo.c
+++ demo.c
@@ -9,7 +9,6 @@ int main(int argc, char **argv)
 	fgets(name, 255, stdin);
 	printf("length = %d\n", strlen(name)); /* debug line */
 	name[strlen(name) - 1] = '\0'; /* remove the newline at the end */
-
 	printf("Hello %s!\n", name);
 	return 0;
 }
